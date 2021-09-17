8016c99a103cb488c10b6fca64067e9bb3138881
Jatin <bhattjatinh@gmail.com>
2021-09-17 14:25:05 +0100

    Initial commit

---
+++ src/Exercise.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+public class Exercise {
+
+    public static void main(String[] args) {
+        int[] array = {1,4,1,4,2,1,3,5,6,2,3,7};
+        Arrays.sort(array);
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
+
+
+    }
+
+}
